89c4a5f51078771ab968224c17ae50d7b7e35854
Jim Ray <ray.jim@gmail.com>
2019-10-24 17:14:02 -0400

    updating TF scripts

--- docker/terraform/templates/bootstrap.sh
+++ docker/terraform/templates/bootstrap.sh
@@ -14,3 +14,11 @@
 
 # install or upgrade required utilities
 yum install -y nano unzip net-tools nmap docker
+
+# docker compose install
+
+yum install -y python-dev py-pip libffi-dev openssl-dev gcc libc-dev make
+
+curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+chmod +x /usr/local/bin/docker-compose

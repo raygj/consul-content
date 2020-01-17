6fca4532aa389c96da06e48f6d697288c9610f28
Jim Ray <ray.jim@gmail.com>
2020-01-17 15:27:03 -0500

    Update bootstrap.sh

--- docker/terraform/templates/bootstrap.sh
+++ docker/terraform/templates/bootstrap.sh
@@ -4,16 +4,13 @@
 # nano setup.sh
 # chmod +x setup.sh
 # ./setup.sh
-# Once the script is complete you should be able to start Consul:
-#
-# systemctl start consul
 
 # snap remove docker
 # apt-get update
 # apt-get install -y unzip nano net-tools nmap socat docker
 
 # install or upgrade required utilities
-yum install -y nano unzip net-tools nmap docker
+yum install -y nano unzip net-tools nmap docker socat
 
 # docker compose install
 

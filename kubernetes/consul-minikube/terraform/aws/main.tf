241f585adca4e2f7e8a12af9f2a00e8c4750e741
Jim <ray.jim@gmail.com>
2020-02-20 14:17:31 -0500

    Update main.tf

--- kubernetes/consul-minikube/terraform/aws/main.tf
+++ kubernetes/consul-minikube/terraform/aws/main.tf
@@ -53,8 +53,8 @@ resource "aws_security_group" "ingress-all-test" {
       "0.0.0.0/0", // any-any rule to specific IP of your host machine <not the world!>
     ]
 
-    from_port = 22
-    to_port   = 22
+    from_port = 0
+    to_port   = 0
     protocol  = "tcp"
   }
 

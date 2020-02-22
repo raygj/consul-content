0038d9ca7fa95e4edfae528223d62e394801aa3f
Jim <ray.jim@gmail.com>
2020-02-21 22:28:34 -0500

    Update main.tf

--- kubernetes/consul-minikube/terraform/aws/main.tf
+++ kubernetes/consul-minikube/terraform/aws/main.tf
@@ -83,7 +83,7 @@ resource "aws_instance" "test-ec2-instance" {
   }
   provisioner "local-exec" {
     command = <<EOF
-  curl https://github.com/raygj/consul-content/blob/master/kubernetes/consul-minikube/terraform/aws/files/bootstrap.sh
+  curl -O https://github.com/raygj/consul-content/blob/master/kubernetes/consul-minikube/terraform/aws/files/bootstrap.sh
   chmod +x ~/bootstrap.sh
   EOF
   }
